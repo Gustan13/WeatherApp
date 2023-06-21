c0d3a0891443f98f373d05910ae9417c6cea8061
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-06-21 08:54:05 -0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Gustavo Binder on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
